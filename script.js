ea3157c5c794b60aee3544d780e68f4ce6386385
unknown <Alpha-Pepper>
2019-08-06 20:36:24 +0300

    Исправление

--- script.js
+++ script.js
@@ -3,29 +3,36 @@
 let arr = [2, -5, 1, -2, 0, -3, 2, -14];
 
 function aboveZero(number){
-    return number > 0;
+    return number >= 0;
 };
 let positiveArr = arr.filter(aboveZero);
 
-let result = positiveArr.map(x => x*x);
+let result = positiveArr.map(function(x){
+    return Math.round(Math.sqrt(x) * 100) / 100;
+});
 
 console.log(result);
 
 let arr2 = [4, 3, 5, 5, 0, 4, 8, 6];
 
 let result2 = arr2.reduce(function(sum, current){
-   for( let a = 0; a < arr2.length; a++){
-    arr2.length = 4 ;
+   if(current === 0){
+       arr2.splice(0);
 }
     return sum + current;
-}, 0);
+}, 0)
+    
 
 console.log(result2);
 
-function summArguments(sum, current){
+function summArguments(){
+    let sum = 0;
     for(let i = 0; i < arguments.length; i++){
-        console.log(arguments[i] = sum + current);
+        sum += arguments[i];
     }
     console.log(sum);
 }
-summArguments(10,10,10,10);
+summArguments(10,10);
+summArguments(10);
+
+
